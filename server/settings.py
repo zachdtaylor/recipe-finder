b94300eadc0b11a572c95dd4bd9de205e6c49e7e
zacharytaylor <zachtylr04@gmail.com>
2019-02-09 21:17:01 -0700

    Decide to use AWS instead of Heroku; uncomment corsheaders stuff.

--- server/settings.py
+++ server/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     # Project Apps Listed First
     'users.apps.UserAppConfig',
     'recipe',
- #   'corsheaders',
+    'corsheaders',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -73,7 +73,7 @@ GRAPHQL_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-#    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -158,7 +158,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
